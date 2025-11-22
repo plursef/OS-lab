f56456a1f02cf1f5cf7a94fa80bdfa1f3f6d78c3
SakuraAir <2300012913@stu.pku.edu.cn>
2025-11-22 18:57:50 +0800

    Exercise 7 should be passed.

--- kern/syscall.c
+++ kern/syscall.c
@@ -82,6 +82,21 @@ sys_exofork(void)
 	// status is set to ENV_NOT_RUNNABLE, and the register set is copied
 	// from the current environment -- but tweaked so sys_exofork
 	// will appear to return 0.
+	struct Env* env;
+	int errno;
+	// call env_alloc to create a blank env with kernel's page mappings
+	if (errno = env_alloc(&env, curenv->env_id) < 0) {
+		cprintf("[%08x]sys_exofork failed: %e\n", curenv->env_id, errno);
+		return errno;
+	}
+	// status is set to ENV_NOT_RUNNABLE
+	// and register set is copied from the current enviroment
+	env->env_status = ENV_NOT_RUNNABLE;
+	env->env_tf = curenv->env_tf;
+	// but tweaked with return value 0
+	env->env_tf.tf_regs.reg_eax = 0;
+
+	return env->env_id;
 
 	// LAB 4: Your code here.
 	panic("sys_exofork not implemented");
@@ -102,6 +117,20 @@ sys_env_set_status(envid_t envid, int status)
 	// You should set envid2env's third argument to 1, which will
 	// check whether the current environment has permission to set
 	// envid's status.
+	struct Env* env = NULL;
+	int errno;
+	// ensure valid env
+	if (errno = envid2env(envid, &env, 1) < 0) {
+		cprintf("environment envid %08x doesn't currently exist\n", envid);
+		return errno;
+	}
+	if (status != ENV_RUNNABLE || status != ENV_NOT_RUNNABLE) {
+		cprintf("[%08x]sys_env_set_status: can't set invalid status(%d)\n", envid, status);
+		return -E_INVAL;
+	}
+	// set enviroment status and return 0 on successfully complete
+	env->env_status = status;
+	return 0;
 
 	// LAB 4: Your code here.
 	panic("sys_env_set_status not implemented");
@@ -148,8 +177,43 @@ sys_page_alloc(envid_t envid, void *va, int perm)
 	//   If page_insert() fails, remember to free the page you
 	//   allocated!
 
+	struct Env* env = NULL;
+	int errno;
+	// ensure valid env
+	if (errno = envid2env(envid, &env, 1) < 0) {
+		cprintf("environment envid %08x doesn't currently exist\n", envid);
+		return errno;
+	}
+	// ensure valid va
+	if (va >= UTOP || ((uintptr_t)va & (PGSIZE - 1))) {
+		cprintf("[%08x]sys_page_alloc: can't alloc at invalid virtual address(%p)\n", envid, va);
+		return -E_INVAL;
+	}
+	// ensure valid perm
+	if (perm & ~PTE_SYSCALL || ((perm & (PTE_U|PTE_P)) != (PTE_U|PTE_P))) {
+		cprintf("[%08x]sys_page_alloc: can't alloc with invalid perm, which violates permission restrictions, PTE_P | PTE_U must be covered, and only 4 bits could be set(PTE_AVAIL | PTE_P | PTE_W | PTE_U)\n", envid, va);
+		return -E_INVAL;
+
+	}
+	// Now we can try alloc
+	struct PageInfo *pp = page_alloc(ALLOC_ZERO);
+	// if no enough memory
+	if (!pp) {
+		cprintf("[%08x]sys_page_alloc: no enough memory for alloc\n", envid);
+		return -E_NO_MEM;
+	}
+	// insert this pp into 'va'
+	if ((errno = page_insert(env->env_pgdir, pp, va, perm | PTE_P | PTE_U)) < 0) {
+		cprintf("[%08x]sys_page_alloc: no enough memory for alloc page table\n", envid);
+		// pp->pp_ref is always 0 because page_alloc doesn't add it
+		page_free(pp);
+		return -E_NO_MEM;
+	}
+	// We have done necessary works, just return 0 to report a good day
+	return 0;
 	// LAB 4: Your code here.
-	panic("sys_page_alloc not implemented");
+	// comment this panic for my confidence
+	// panic("sys_page_alloc not implemented");
 }
 
 // Map the page of memory at 'srcva' in srcenvid's address space
@@ -179,8 +243,29 @@ sys_page_map(envid_t srcenvid, void *srcva,
 	//   Use the third argument to page_lookup() to
 	//   check the current permissions on the page.
 
+	// ensure valid perm
+	if ( (perm & (PTE_U|PTE_P))  !=  (PTE_U|PTE_P)  ) return -E_INVAL;
+	if ( (perm | PTE_SYSCALL) != PTE_SYSCALL  ) return -E_INVAL;
+	// ensure valid va
+	if ((uintptr_t)srcva >= UTOP || PGOFF(srcva) != 0) return -E_INVAL;
+	if ((uintptr_t)dstva >= UTOP || PGOFF(dstva) != 0) return -E_INVAL;
+
+	struct Env *srcenv, *dstenv;
+	// ensure valid env
+	if (envid2env(srcenvid, &srcenv, 1)<0 || envid2env(dstenvid, &dstenv, 1)<0) return -E_BAD_ENV;
+
+	pte_t *src_pte;  
+	struct PageInfo *pp = page_lookup(srcenv->env_pgdir, srcva, &src_pte);
+	// if srcva is not mapped in srcenvid's address space, return -E_INVAL
+	if (pp==NULL) return -E_INVAL;
+	// -E_INVAL if (perm & PTE_W), but srcva is read-only in srcenvid's address space.
+	if (((*src_pte & PTE_W) == 0) && ((perm & PTE_W) == PTE_W )) return -E_INVAL;
+	int errno = page_insert(dstenv->env_pgdir, pp, dstva, perm);
+	// if no memory
+	if(errno < 0) return errno;
+	return 0;
 	// LAB 4: Your code here.
-	panic("sys_page_map not implemented");
+	// panic("sys_page_map not implemented");
 }
 
 // Unmap the page of memory at 'va' in the address space of 'envid'.
@@ -194,9 +279,23 @@ static int
 sys_page_unmap(envid_t envid, void *va)
 {
 	// Hint: This function is a wrapper around page_remove().
+	struct Env* env = NULL;
+	int errno;
+	// ensure valid env
+	if (errno = envid2env(envid, &env, 1) < 0) {
+		cprintf("environment envid %08x doesn't currently exist\n", envid);
+		return errno;
+	}
+
+	// ensure valid va
+	if (va >= UTOP || ((uintptr_t)va & (PGSIZE - 1))) {
+		cprintf("[%08x]sys_page_alloc: can't alloc at invalid virtual address(%p)\n", envid, va);
+		return -E_INVAL;
+	}
+	page_remove(env->env_pgdir, va);
 
 	// LAB 4: Your code here.
-	panic("sys_page_unmap not implemented");
+	// panic("sys_page_unmap not implemented");
 }
 
 // Try to send 'value' to the target env 'envid'.
@@ -283,6 +382,16 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 		return sys_getenvid();
 	case SYS_env_destroy:
 		return sys_env_destroy((envid_t)a1);
+	case SYS_page_alloc:
+		return sys_page_alloc((envid_t)a1, (void *)a2, (int)a3);
+	case SYS_page_map:
+		return sys_page_map((envid_t)a1, (void *)a2, (envid_t)a3, (void *)a4, (int)a5);
+	case SYS_page_unmap:
+		return sys_page_unmap((envid_t)a1, (void *)a2);
+	case SYS_exofork:
+		return sys_exofork();
+	case SYS_env_set_status:
+		return sys_env_set_status((envid_t)a1, (int)a2);
 	case SYS_yield:
 		sys_yield(); return 0; // sys_yield never returns, but we return 0 to satisfy the compiler and my mind
 	default:
